94eabd5f775505b0ad3e5aaa30a007beced379ab
qweeze <qweeeze@gmail.com>
2022-03-19 18:46:24 +0500

    Change QPID to Azure AMQP

--- tests/test_amqp.py
+++ tests/test_amqp.py
@@ -1,4 +1,5 @@
 import pytest
+import uamqp.message
 
 from rstream import (
     AMQPMessage,
@@ -12,8 +13,8 @@ pytestmark = pytest.mark.asyncio
 
 async def test_amqp_message(stream: str, consumer: Consumer, producer: Producer) -> None:
     amqp_message = AMQPMessage(
-        properties={"subject": "test-subject"},
-        annotations={"test": 42},
+        properties=uamqp.message.MessageProperties(subject=b"test-subject"),
+        annotations={b"test": 42},
         body="test-body",
     )
     await producer.publish(stream, amqp_message)
@@ -29,6 +30,6 @@ async def test_amqp_message(stream: str, consumer: Consumer, producer: Producer)
     await consumer.run()
 
     assert isinstance(incoming_amqp_message, AMQPMessage)
-    assert incoming_amqp_message.body == amqp_message.body
-    assert incoming_amqp_message.properties == amqp_message.properties
+    assert list(incoming_amqp_message.get_data()) == list(amqp_message.get_data())
+    assert incoming_amqp_message.properties.subject == amqp_message.properties.subject
     assert incoming_amqp_message.annotations == amqp_message.annotations
