9b0296cc652f116ab26b324541384c31ac4b8f8b
Cameron Jones <cameron.jones@vaisala.com>
2022-04-20 20:38:00 +0300

    isort complaining

--- rstream/__init__.py
+++ rstream/__init__.py
@@ -10,10 +10,10 @@ except metadata.PackageNotFoundError:
 del metadata
 
 
-from .amqp import AMQPMessage, amqp_decoder     # noqa: E402
-from .constants import OffsetType               # noqa: E402
-from .consumer import Consumer                  # noqa: E402
-from .producer import Producer, RawMessage      # noqa: E402
+from .amqp import AMQPMessage, amqp_decoder  # noqa: E402
+from .constants import OffsetType  # noqa: E402
+from .consumer import Consumer  # noqa: E402
+from .producer import Producer, RawMessage  # noqa: E402
 
 __all__ = [
     "AMQPMessage",
