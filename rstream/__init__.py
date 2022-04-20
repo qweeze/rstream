9d00cea1f3472537f5a366bf6c91b3241fda7373
Cameron Jones <cameron.jones@vaisala.com>
2022-04-20 20:35:12 +0300

    Safer versioning for dev, ignore flake8 import errors

--- rstream/__init__.py
+++ rstream/__init__.py
@@ -1,14 +1,19 @@
 from importlib import metadata
 
-__version__ = metadata.version(__package__)
-__license__ = metadata.metadata(__package__)["license"]
+try:
+    __version__ = metadata.version(__package__)
+    __license__ = metadata.metadata(__package__)["license"]
+except metadata.PackageNotFoundError:
+    __version__ = "dev"
+    __license__ = None
+
 del metadata
 
 
-from .amqp import AMQPMessage, amqp_decoder
-from .constants import OffsetType
-from .consumer import Consumer
-from .producer import Producer, RawMessage
+from .amqp import AMQPMessage, amqp_decoder     # noqa: E402
+from .constants import OffsetType               # noqa: E402
+from .consumer import Consumer                  # noqa: E402
+from .producer import Producer, RawMessage      # noqa: E402
 
 __all__ = [
     "AMQPMessage",
