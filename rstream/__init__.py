1aaf5d6842888051d616e986dd3a85225f8053e1
qweeze <qweeeze@gmail.com>
2022-05-11 21:35:13 +0500

    move imports to the top of the file

--- rstream/__init__.py
+++ rstream/__init__.py
@@ -1,5 +1,10 @@
 from importlib import metadata
 
+from .amqp import AMQPMessage, amqp_decoder
+from .constants import OffsetType
+from .consumer import Consumer
+from .producer import Producer, RawMessage
+
 try:
     __version__ = metadata.version(__package__)
     __license__ = metadata.metadata(__package__)["license"]
@@ -8,12 +13,6 @@ except metadata.PackageNotFoundError:
     __license__ = None
 
 del metadata
-
-
-from .amqp import AMQPMessage, amqp_decoder  # noqa: E402
-from .constants import OffsetType  # noqa: E402
-from .consumer import Consumer  # noqa: E402
-from .producer import Producer, RawMessage  # noqa: E402
 
 __all__ = [
     "AMQPMessage",
