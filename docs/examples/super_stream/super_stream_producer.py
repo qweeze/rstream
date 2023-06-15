f14598b95a0a1df1c7ba2e516e8badc898a1fb2f
Gabriele Santomaggio <G.santomaggio@gmail.com>
2023-06-15 17:04:12 +0200

    Add the example for super stream
    Signed-off-by: Gabriele Santomaggio <G.santomaggio@gmail.com>

--- docs/examples/super_stream/super_stream_producer.py
+++ docs/examples/super_stream/super_stream_producer.py
@@ -1,4 +1,5 @@
 import asyncio
+import time
 
 from rstream import (
     AMQPMessage,
@@ -13,12 +14,12 @@ async def routing_extractor(message: AMQPMessage) -> str:
 
 async def publish():
     async with SuperStreamProducer(
-        "localhost",
-        username="guest",
-        password="guest",
-        routing_extractor=routing_extractor,
-        routing=RouteType.Hash,
-        super_stream="invoices",
+            "localhost",
+            username="guest",
+            password="guest",
+            routing_extractor=routing_extractor,
+            routing=RouteType.Hash,
+            super_stream="invoices",
     ) as super_stream_producer:
         for i in range(100):
             amqp_message = AMQPMessage(
@@ -26,6 +27,10 @@ async def publish():
                 application_properties={"id": "{}".format(i)},
             )
             await super_stream_producer.send(amqp_message)
+        print("Published 100 messages to super stream: invoices")
+
+        # wait a bit to be sure that the messages are delivered
+        await asyncio.sleep(1)
 
 
 asyncio.run(publish())
