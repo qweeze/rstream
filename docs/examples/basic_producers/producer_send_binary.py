efa1de325f560313f8f5784c19c64b7e9bb2edcc
Gabriele Santomaggio <G.santomaggio@gmail.com>
2023-07-07 15:33:26 +0200

    Add performances documentation (#91)
    Signed-off-by: Gabriele Santomaggio <G.santomaggio@gmail.com>

--- docs/examples/basic_producers/producer_send_binary.py
+++ docs/examples/basic_producers/producer_send_binary.py
@@ -1,4 +1,5 @@
 import asyncio
+import time
 
 from rstream import AMQPMessage, Producer
 
@@ -14,9 +15,14 @@ async def publish():
         # sending a million of messages in binary format
         # note that this is not compatible with other clients (e.g. Java,.NET)
         # since they expect messages in AMQP 1.0
+        start_time = time.perf_counter()
+
         for i in range(MESSAGES):
             # send is asynchronous
             await producer.send(stream=STREAM, message=b"hello")
+
+        end_time = time.perf_counter()
+        print(f"Sent {MESSAGES} messages in {end_time - start_time:0.4f} seconds")
 
 
 asyncio.run(publish())
