3076c8662626dedb45ee68be07208e9ad6b0aed8
Daniele <dpalaia@vmware.com>
2023-07-10 11:20:11 +0200

    fixing a bug in send_sub_entry_batching (#93)
    * fixing a bug in send_sub_entry_batching
    
    * change sub entry example
    
    Signed-off-by: Gabriele Santomaggio <G.santomaggio@gmail.com>
    
    * bump to 0.10.2
    
    Signed-off-by: Gabriele Santomaggio <G.santomaggio@gmail.com>
    
    ---------
    
    Signed-off-by: Gabriele Santomaggio <G.santomaggio@gmail.com>
    Co-authored-by: Gabriele Santomaggio <G.santomaggio@gmail.com>

--- docs/examples/sub_entry_batch/producer_sub_entry_batch.py
+++ docs/examples/sub_entry_batch/producer_sub_entry_batch.py
@@ -4,28 +4,38 @@ import time
 from rstream import AMQPMessage, CompressionType, Producer
 
 STREAM = "my-test-stream"
-MESSAGES = 100
+LOOP = 5000
+BATCH = 100
 
 
 async def publish():
     async with Producer("localhost", username="guest", password="guest") as producer:
         await producer.create_stream(STREAM, exists_ok=True)
+        start_time = time.perf_counter()
 
-        messages = []
-        for i in range(MESSAGES):
-            amqp_message = AMQPMessage(
-                body="a:{}".format(i),
+        print("Sending compressed and not compressed messages with sub_entry")
+
+        for j in range(LOOP):
+            messages = []
+            for i in range(BATCH):
+                amqp_message = AMQPMessage(
+                    body="a:{}".format(i),
+                )
+                messages.append(amqp_message)
+
+            # sending with compression
+            await producer.send_sub_entry(
+                STREAM, compression_type=CompressionType.Gzip, sub_entry_messages=messages
             )
-            messages.append(amqp_message)
 
-        # sending with compression
-        await producer.send_sub_entry(
-            STREAM, compression_type=CompressionType.Gzip, sub_entry_messages=messages
-        )
-
-        # sending without compression
-        await producer.send_sub_entry(
-            STREAM, compression_type=CompressionType.No, sub_entry_messages=messages
+            # sending without compression
+            await producer.send_sub_entry(
+                STREAM, compression_type=CompressionType.No, sub_entry_messages=messages
+            )
+        end_time = time.perf_counter()
+        print(
+            f"Sent {LOOP * BATCH * 2} messages in {end_time - start_time:0.4f} seconds {LOOP * BATCH} "
+            f"compressed and {LOOP * BATCH} not compressed"
         )
 
     await producer.close()
