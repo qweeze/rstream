952e2634949273af3d73c7936882d85369ba20d0
Gabriele Santomaggio <G.santomaggio@gmail.com>
2023-07-17 14:27:28 +0200

    Add counters
    Signed-off-by: Gabriele Santomaggio <G.santomaggio@gmail.com>

--- docs/examples/super_stream/super_stream_producer.py
+++ docs/examples/super_stream/super_stream_producer.py
@@ -10,6 +10,7 @@ from rstream import (
 SUPER_STREAM = "invoices"
 MESSAGES = 1000000
 
+
 # this value will be hashed using mumh3 hashing algorithm to decide the partition resolution for the message
 async def routing_extractor(message: AMQPMessage) -> str:
     return message.application_properties["id"]
@@ -26,13 +27,21 @@ async def publish():
         super_stream=SUPER_STREAM,
     ) as super_stream_producer:
         # Sending a million messages
+
+        start_time = time.perf_counter()
         for i in range(MESSAGES):
             amqp_message = AMQPMessage(
                 body="hello: {}".format(i),
                 application_properties={"id": "{}".format(i)},
             )
             await super_stream_producer.send(amqp_message)
-        print("Published 1000000 messages to super stream: invoices")
+            if i % 100000 == 0:
+                print(f"Published {i} messages to super stream: {SUPER_STREAM}")
+
+        end_time = time.perf_counter()
+        print(
+            f"Sent {MESSAGES} messages in {end_time - start_time:0.4f} seconds to super stream: {SUPER_STREAM}"
+        )
 
 
 asyncio.run(publish())
--- docs/examples/super_stream/super_stream_producer_key.py
+++ docs/examples/super_stream/super_stream_producer_key.py
@@ -10,6 +10,7 @@ from rstream import (
 SUPER_STREAM = "invoices"
 MESSAGES = 1000000
 
+
 # routing key and the bindings between the super stream exchange and the streams
 async def routing_extractor(message: AMQPMessage) -> str:
     return "key1"
@@ -33,9 +34,13 @@ async def publish():
                 application_properties={"id": "{}".format(i)},
             )
             await super_stream_producer.send(amqp_message)
-        print("Published 1000000 messages to super stream: invoices")
+            if i % 100000 == 0:
+                print(f"Published {i} messages to super stream: {SUPER_STREAM}")
+
         end_time = time.perf_counter()
-        print(f"Sent {MESSAGES} messages in {end_time - start_time:0.4f} seconds")
+        print(
+            f"Sent {MESSAGES} messages in {end_time - start_time:0.4f} seconds to super stream: {SUPER_STREAM}"
+        )
 
 
 asyncio.run(publish())
