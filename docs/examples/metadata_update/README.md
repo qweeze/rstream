93279a14c0419c54e54cda65ad1b0338ffd42af1
Daniele Palaia <dpalaia@vmware.com>
2024-02-14 14:20:25 +0100

    removing and fix examples

--- docs/examples/metadata_update/README.md
+++ docs/examples/metadata_update/README.md
@@ -1,9 +1,11 @@
 Simple scenarios for Metadata update management
 ---
 
-If the streams topology changes (ex:Stream deleted or add/remove follower), the client receives an MetadataUpdate event.
+If the streams topology changes (ex:Stream deleted or add/remove follower), the client receives a MetadataUpdate event.
 
-The client is notified by a callback (similar to what happens for connection_broken scenarios)
+The server removes the producers and consumers linked to the stream before sending the Metadata update event.
+
+Similarly to the disconnection scenario the Producer tries to automatically reconnect while the Consumer gets notified by a callback
 
 After this the client can try to reconnect.
 
@@ -13,16 +15,8 @@ You can start the producer and the consumer and then force the server to change 
 
 rabbitmq-streams delete_replica my-test-stream rabbit@rabbitmqcluster-sample-server-0.rabbitmqcluster-sample-nodes.rabbitmq-system
 
-The examples need to run in a minimal 3 nodes RabbitMQ cluster.
-
-The server removes the producers and consumers linked to the stream before sending the Metadata update event.
-It is up to the user to decide what to do. 
-For example:
-  - Reconnect to producer/consumer 
-  - Close the producer/consumer 
-  - Check if the stream still exists and reconnect the producer /consumer
-
 During the reconnection or the check you could receive the `streamNotAvaiable` error.
 `streamNotAvaiable` is a temporary problem. It means that the stream is not ready yet. 
 
-There is also an example where you can manage both Metadata Update and disconnections
+Producer/Superstream-producer side there is nothing really much to do (the Producer tries to reconnect automatically).
+Consumer side instead you need to define a callback to catch the event and decide what to do.
--- docs/examples/metadata_update/super_stream_producer_metadata_update.py
+++ docs/examples/metadata_update/super_stream_producer_metadata_update.py
@@ -5,11 +5,12 @@ from rstream import (
     AMQPMessage,
     OnClosedErrorInfo,
     RouteType,
+    StreamDoesNotExist,
     SuperStreamProducer,
 )
 
 SUPER_STREAM = "invoices"
-MESSAGES = 10000000
+MESSAGES = 100000000
 producer_closed = False
 
 # this value will be hashed using mumh3 hashing algorithm to decide the partition resolution for the message
@@ -18,44 +19,16 @@ async def routing_extractor(message: AMQPMessage) -> str:
 
 
 async def publish():
-    async def on_metadata_update(on_closed_info: OnClosedErrorInfo) -> None:
-
-        global producer_closed
-        producer_closed = True
-
-        if on_closed_info.reason == "MetaData Update":
-            print(
-                "metadata changed for stream : "
-                + str(on_closed_info.streams[0])
-                + " with code: "
-                + on_closed_info.reason
-            )
-            await asyncio.sleep(2)
-            # reconnect just if the partition exists
-            if await super_stream_producer.stream_exists((on_closed_info.streams[0])):
-                await super_stream_producer.reconnect_stream(on_closed_info.streams[0])
-
-        else:
-            print(
-                "connection has been closed from stream: "
-                + str(on_closed_info.streams)
-                + " for reason: "
-                + str(on_closed_info.reason)
-            )
-            await asyncio.sleep(2)
-            # reconnect just if the partition exists
-            for stream in on_closed_info.streams:
-                await super_stream_producer.reconnect_stream(stream)
 
     # SuperStreamProducer wraps a Producer
     async with SuperStreamProducer(
         "localhost",
-        username="guest",
-        password="guest",
+        username="test",
+        password="test",
         routing_extractor=routing_extractor,
         routing=RouteType.Hash,
         super_stream=SUPER_STREAM,
-        on_close_handler=on_metadata_update,
+        load_balancer_mode=True,
     ) as super_stream_producer:
         # Sending a million messages
 
@@ -66,18 +39,21 @@ async def publish():
                 application_properties={"id": "{}".format(i)},
             )
             global producer_closed
+
             if producer_closed is False:
                 try:
                     await super_stream_producer.send(amqp_message)
                 except Exception as e:
                     # give some time to the reconnect_stream to reconnect
-                    await asyncio.sleep(5)
+                    print("error sending message: {}".format(e))
+                    producer_closed = False
                     continue
             else:
                 producer_closed = False
                 continue
             if i % 100000 == 0:
                 print(f"Published {i} messages to super stream: {SUPER_STREAM}")
+                await asyncio.sleep(2)
 
         end_time = time.perf_counter()
         print(
